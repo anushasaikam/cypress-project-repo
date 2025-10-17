3d7a27ca5288cded538f71cc3791d0a70f67d922
anushasaikam <asaikam25@gmail.com>
2025-10-18 00:20:51 +0100

    Feature/test1 (#4)
    * change1
    
    * change2
    
    * change3
    
    * change4

--- cypress/e2e/pages/LoginPage.js
+++ cypress/e2e/pages/LoginPage.js
@@ -11,5 +11,7 @@ export class LoginPage {
     this.username().clear().type(user);
     this.password().clear().type(pass, { log: false });
     this.loginBtn();
+    console.log("Typing password");
+    this.loginBtn().click();
   }
 }
